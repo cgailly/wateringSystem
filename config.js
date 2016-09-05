7e6f7a5f79e39c18bae16f06a96333bde9ffc022
1324994 <omary.lhoussine@gmail.com>
2016-09-05 13:24:08 +0200

    change IP

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ config.web = {};
 config.Blynk = {};
 config.web.port = 8081;
 config.Blynk.port = 8080;
-config.Blynk.server = "40.127.133.115";
+config.Blynk.server = "13.74.253.190";
 config.Blynk.authToken = "b53c5fe6c4494e74be19950dda122723";
 
 config.mongoDb = {};
